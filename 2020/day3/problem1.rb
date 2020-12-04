7fd66f259c349d9be6bfccb54cdbff5d7cd78d28
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-12-03 19:56:52 -0700

    small refactor

---
+++ 2020/day3/problem1.rb
@@ -0,0 +1,32 @@
+idx = 0
+key = 1
+length = 0
+trees = 0
+spaces = 0
+count = 0
+grid = Hash.new
+
+File.open("input.txt", "r") do |f|
+    f.each_line do |line|
+        grid[count] = line.gsub("\n", "")
+        count += 1
+        length = line.length
+    end
+end
+
+grid.each do |key, value|
+    if idx < grid[key].length
+      new_position = grid[key][idx]
+      new_position == '.' ? spaces += 1 : trees += 1
+    else
+      idx = idx - length
+      new_position = grid[key][idx]
+      new_position == '.' ? spaces += 1 : trees += 1
+    end
+    idx += 3
+    key += 1
+    trees
+    spaces
+end
+  
+p trees
